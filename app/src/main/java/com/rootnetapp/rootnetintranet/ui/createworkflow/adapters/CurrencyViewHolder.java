7cc37cc39424dbbad8ce21bbb56bcf414639cc3d
Lucas Cabrales <lcabrales@rootstack.com>
2018-11-21 16:06:08 -0500

    Define layout for Currency form item
    Create the XML file and the corresponding ViewHolder with data binding.

---
+++ app/src/main/java/com/rootnetapp/rootnetintranet/ui/createworkflow/adapters/CurrencyViewHolder.java
@@ -0,0 +1,16 @@
+package com.rootnetapp.rootnetintranet.ui.createworkflow.adapters;
+
+import com.rootnetapp.rootnetintranet.databinding.FormItemCurrencyBinding;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CurrencyViewHolder extends RecyclerView.ViewHolder {
+
+    private FormItemCurrencyBinding mBinding;
+
+    public CurrencyViewHolder(FormItemCurrencyBinding binding) {
+        super(binding.getRoot());
+
+        mBinding = binding;
+    }
+}

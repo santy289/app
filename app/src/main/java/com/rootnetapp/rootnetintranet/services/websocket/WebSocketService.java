33ad4767f9b6690f35540888bf2fdfadd9bfeea0
Lucas Cabrales <lcabrales@rootstack.com>
2019-03-29 10:32:27 -0500

    RIM-59 Add Firebase to the project

--- app/src/main/java/com/rootnetapp/rootnetintranet/services/websocket/WebSocketService.java
+++ app/src/main/java/com/rootnetapp/rootnetintranet/services/websocket/WebSocketService.java
@@ -112,6 +112,7 @@ public class WebSocketService extends Service {
 
     @Override
     public void onDestroy() {
+        //todo GoogleAnalytics
 //        Toast.makeText(getApplicationContext(), "onDestroy Service " + startId , Toast.LENGTH_LONG).show();
         Log.d(TAG, "onDestroy: SERVICE DESTROYED");
         serviceCleanup();
